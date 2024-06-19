98cb1986aa4bd865e9537df8ec8b059ba9c1080a
hardikjoshi746 <joshi.hard@northeastern.edu>
2024-06-19 00:34:41 -0400

    Added server side validation using Joi API

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const methodOverride = require('method-override');
 const catchAsync = require('./util/catchAsync');
 const ExpressError = require('./util/expressError')
 const ejsMate = require('ejs-mate') 
+const { campgroundSchema } = require('./schemas.js')
 const Campground = require('./models/campground');
 const { sourceMapsEnabled } = require('process');
 
@@ -26,6 +27,16 @@ app.set('views', path.join(__dirname, 'views')) // where to find ejs files
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
 
+const validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body)
+    if(error){
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else{
+        next()
+    }
+}
+
 // for home page
 app.get('/', (req, res) => {
     res.render('home')
@@ -41,8 +52,8 @@ app.get('/campgrounds/new',  (req, res) => {
     res.render('campgrounds/new')
 })
 
-app.post('/campgrounds', catchAsync(async (req, res, next) => {
-    if(req.body.campground) throw new ExpressError()
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
+    // if(req.body.campground) throw new ExpressError()
     const campground = new Campground(req.body.campground)
     await campground.save()
     res.redirect(`/campgrounds/${campground._id}`)
@@ -61,7 +72,7 @@ app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     res.render('campgrounds/edit', { campground })
 }))
 
-app.put('/campgrounds/:id', catchAsync(async (req, res) => {
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
     const {id} = req.params
     const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
     res.redirect(`/campgrounds/${campground._id}`)
